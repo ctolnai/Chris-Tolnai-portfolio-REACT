fd21b3c57ec8d0faacea1dd4079492ad32f14535
Chris Tolnai <christolnai@gmail.com>
2021-10-29 12:29:27 -0700

    updated project 1

--- src/components/pages/p1.js
+++ src/components/pages/p1.js
@@ -1,17 +1,44 @@
 import React from 'react';
-import { Card, Button, Row } from 'react-bootstrap/';
+import { Card, Button, Row, Col, ListGroup } from 'react-bootstrap/';
 
 export default function P1() {
   return (
     <Row>
-      <Card className="text-center">
-        <Card.Header>ParkSmart</Card.Header>
-        <Card.Img variant="top" src="./assets/images/parksmart.png" />
-        <Card.Body>
-          <Button variant="primary" href="https://github.com/Kimberly-Rodriguez/ParkSmart" target="_blank">Github Repo</Button>
-          <Button variant="primary" href="https://parksmart2021.herokuapp.com/" target="_blank">Deployed Application</Button>
-        </Card.Body>
-      </Card>
+      <Col>
+        <Card style={{ width: '30rem' }}>
+          <Card.Header style={{ fontSize: "22px" }}>Description</Card.Header>
+          <ListGroup style={{ fontSize: "18px" }} variant="flush">
+            <ListGroup.Item>Users in a neighborhood can create an account on the Park Smart © app. Once they are logged in, they are presented with a map of their neighborhood block with numbered street parking spaces that are color-coded based on if they are taken or available. Spaces that are green are available and spaces that are red are taken. Once a user parks in an available space, they click on the designated number on the app’s map, where they may then choose, “I am parking here” & have the option of adding what time they plan on leaving. When they submit the form, the space will turn red & the ‘time available’ will be included as an attribute users can see when they view the space. Users can also update their ‘leave time’ if they plan on leaving sooner or later than they originally stated. Other users are now able to edit the spot once they have parked there.</ListGroup.Item>
+          </ListGroup>
+          <Card.Header style={{ fontSize: "22px" }}>Technologies</Card.Header>
+          <ListGroup style={{ fontSize: "18px" }} variant="flush">
+            <ListGroup.Item>html</ListGroup.Item>
+            <ListGroup.Item>css</ListGroup.Item>
+            <ListGroup.Item>javascript</ListGroup.Item>
+            <ListGroup.Item>bootstrap</ListGroup.Item>
+            <ListGroup.Item>sequelize</ListGroup.Item>
+            <ListGroup.Item>mysql2</ListGroup.Item>
+            <ListGroup.Item>bcrypt</ListGroup.Item>
+            <ListGroup.Item>express</ListGroup.Item>
+            <ListGroup.Item>handlebars</ListGroup.Item>
+            <ListGroup.Item>moment.js</ListGroup.Item>
+          </ListGroup>
+          <Card.Header style={{ fontSize: "22px" }}>Role</Card.Header>
+          <ListGroup style={{ fontSize: "18px" }} variant="flush">
+          <ListGroup.Item>I did...</ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </Col>
+      <Col>
+        <Card className="text-center">
+          <Card.Header>ParkSmart</Card.Header>
+          <Card.Img variant="top" src="./assets/images/parksmart.png" />
+          <Card.Body>
+            <Button variant="primary" href="https://github.com/Kimberly-Rodriguez/ParkSmart" target="_blank">Github Repo</Button>
+            <Button variant="primary" href="https://parksmart2021.herokuapp.com/" target="_blank">Deployed Application</Button>
+          </Card.Body>
+        </Card>
+      </Col>
     </Row>
   );
 }
--- src/components/pages/projects.js
+++ src/components/pages/projects.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Row, Col, Card } from 'react-bootstrap/';
 import { Link } from 'react-router-dom';
 
+const style ={
+height:"175px"
+}
 
 export default function Projects() {
   return (
@@ -9,7 +12,7 @@ export default function Projects() {
       <Col>
       <Link to={"/p1"}>
         <Card>
-          <Card.Img variant="top" src="./assets/images/parksmart.png" />
+          <Card.Img style={style} variant="top" src="./assets/images/parksmart.png" />
           <Card.Body>
             <Card.Title>ParkSmart</Card.Title>
           </Card.Body>
@@ -17,13 +20,9 @@ export default function Projects() {
         </Link>
         <Link to={"/p2"}>
       <Card>
-        <Card.Img style={{height:"175px"}} variant="top" src="./assets/images/workdayscheduler.png" />
+        <Card.Img style={style} variant="top" src="./assets/images/workdayscheduler.png" />
         <Card.Body>
           <Card.Title>Work Day Scheduler</Card.Title>
-          <Card.Text>
-            This is a longer card with supporting text below as a natural
-            lead-in to additional content. This content is a little bit longer.
-          </Card.Text>
         </Card.Body>
       </Card>
       </Link>
@@ -31,25 +30,17 @@ export default function Projects() {
       <Col>
       <Link to={"/p3"}>
         <Card>
-          <Card.Img variant="top" src="./assets/images/notetaker.png" />
+          <Card.Img style={style} variant="top" src="./assets/images/notetaker.png" />
           <Card.Body>
             <Card.Title>Note Taker</Card.Title>
-            <Card.Text>
-              This is a longer card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit longer.
-            </Card.Text>
           </Card.Body>
         </Card>
         </Link>
         <Link to={"/p4"}>
       <Card>
-        <Card.Img variant="top" src="./assets/images/jobsearch.png" />
+        <Card.Img style={style} variant="top" src="./assets/images/jobsearch.png" />
         <Card.Body>
           <Card.Title>Job Search</Card.Title>
-          <Card.Text>
-            This is a longer card with supporting text below as a natural
-            lead-in to additional content. This content is a little bit longer.
-          </Card.Text>
         </Card.Body>
       </Card>
       </Link>
@@ -57,24 +48,16 @@ export default function Projects() {
       <Col>
       <Link to={"/p5"}>
         <Card>
-          <Card.Img variant="top" src="./assets/images/newsforyou.png" />
+          <Card.Img style={style} variant="top" src="./assets/images/newsforyou.png" />
           <Card.Body>
             <Card.Title>News For You</Card.Title>
-            <Card.Text>
-              This is a longer card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit longer.
-            </Card.Text>
           </Card.Body>
         </Card>
         </Link>
       <Card>
-        <Card.Img variant="top" src="./assets/images/newsforyou.png" />
+        <Card.Img style={style} variant="top" src="./assets/images/newsforyou.png" />
         <Card.Body>
           <Card.Title>News For You</Card.Title>
-          <Card.Text>
-            This is a longer card with supporting text below as a natural
-            lead-in to additional content. This content is a little bit longer.
-          </Card.Text>
         </Card.Body>
       </Card>
       </Col>
